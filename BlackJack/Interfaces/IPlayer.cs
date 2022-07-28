f12576050eab64b2691b49dea3174b985cf0c7f4
OX-ey <108790211+OX-ey@users.noreply.github.com>
2022-07-28 16:25:26 +0200

    Add files via upload

---
+++ BlackJack/Interfaces/IPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BlackJack.Engine;
+using BlackJack.Engine.Classes;
+
+namespace BlackJack.Interfaces
+{
+	public interface IPlayer
+    {
+        public Hand hand { get; set; }
+        public string name { get; set; }
+        public bool bj { get; set; }
+
+
+    }
+}
+
